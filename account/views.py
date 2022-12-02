72b86c998e093ecc5477e18087dcefde3abcbe50
Xiloid101 <vsvivk@gmail.com>
2022-12-02 18:48:46 +0100

    Prepared Sign-Up  registration form

--- account/views.py
+++ account/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import RegistrationForm
 
-# Create your views here.
+#from django.template.loader import render_to_string
+#from django.shortcuts import redirect
+
+def account_register(request):
+
+    if request.user,is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            # Setup email
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject, message=message)
+    else:
+        registerForm = RegistrationForm()
+
+    return render(request, 'account/registration/register.html', {'form': registerForm})
--- store/views.py
+++ store/views.py
@@ -7,14 +7,14 @@ def about_us(request):
     return render(request, 'store/about.html')
 
 def product_all(request):
-    products = Product.products.all()
+    products = Product.objects.filter(is_active=True)
     return render(request, 'store/home.html', {'products': products})
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    product = get_object_or_404(Product, slug=slug, is_active=True)
     return render(request, 'store/products/single.html', {'product': product})
 
 def category_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category=category)
+    products = Product.objects.filter(category=category, is_active=True)
     return render(request, 'store/products/category.html', {'category': category, 'products': products})
