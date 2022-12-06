dad0c6f460bbb97175d3fdaf0610b7a7d566efd5
Xiloid101 <vsvivk@gmail.com>
2022-12-06 17:30:17 +0100

    Finished the account registration form

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,5 @@ app_name = 'account'
 
 urlpatterns = [
     path('register/', views.account_register, name='register'),
+    path('activate/,slug:uidb64>/slug:token>/', views.account_activate, name='activate'),
     ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import RegistrationForm
+from .token import account_activation_token
+from django.template.loader import render_to_string
 
-#from django.template.loader import render_to_string
-#from django.shortcuts import redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 
 def account_register(request):
 
@@ -32,3 +36,6 @@ def account_register(request):
         registerForm = RegistrationForm()
 
     return render(request, 'account/registration/register.html', {'form': registerForm})
+
+
+    def account_activate(request):
